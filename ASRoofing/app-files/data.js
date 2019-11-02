cf4c8b45f090eadf681d3b5ca02e1c26729eca30
kk6axq <kk6axq@users.noreply.github.com>
2019-11-02 16:20:47 -0700

    Add AS Roofing Panorama

---
+++ ASRoofing/app-files/data.js
@@ -0,0 +1,53 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-all-seasons-roofing",
+      "name": "All Seasons Roofing",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3840,
+      "initialViewParameters": {
+        "yaw": -1.1449840952916617,
+        "pitch": 1.2349576906723225,
+        "fov": 1.362234561578609
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.020634004242623405,
+          "pitch": 0.04736838107168495,
+          "title": "A product of Northern Arizona Aerial Photography<br>",
+          "text": "<div>https://bit.ly/northernazap</div><div>©2019 Northern Arizona Aerial Photography LLC<br></div>"
+        }
+      ]
+    }
+  ],
+  "name": "All Seasons Roofing",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
